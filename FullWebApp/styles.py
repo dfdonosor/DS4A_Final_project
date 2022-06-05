71fc589febba012f20873576f67bf6ea08f1b15f
Daniel Donoso R <dfdonosor@unal.edu.co>
2022-06-05 01:39:31 -0500

    Se estruturo la pagina, se creo el header, un sidebar que funciona como menu para renderizar las paginas, y el contenido de cada pagina.

---
+++ FullWebApp/styles.py
@@ -0,0 +1,33 @@
+# the style arguments for the sidebar. We use position:fixed and a fixed width
+CONTAINER_STYLE = {
+    "top": 0, 
+    "left": 0, 
+    "bottom": 0,
+    "height" : "100vh"
+}
+
+HEADER_STYLE = {
+    "position": "static",
+    "color" : "white",
+    "background-color": "#2E487E",
+    "height": "20%"
+}
+
+# the style arguments for the sidebar. We use position:fixed and a fixed width
+SIDEBAR_STYLE = {
+    "position": "static",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+    "height": "100%"
+}
+
+# the styles for the main content position it to the right of the sidebar and
+# add some padding.
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
