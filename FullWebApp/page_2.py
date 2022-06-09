88bf4e0b4bcf346e51ff21c1f6998ae4efd90eb9
Daniel Donoso R <dfdonosor@unal.edu.co>
2022-06-08 21:58:45 -0500

    Se introducen los rangeslider de la page 2

--- FullWebApp/page_2.py
+++ FullWebApp/page_2.py
@@ -8,6 +8,9 @@ import pandas as pd
 import pickle
 from styles import *
 
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+
 page1 = dbc.Container([
 
     dbc.Row(
@@ -15,16 +18,89 @@ page1 = dbc.Container([
                 html.Div([
 
                         dbc.Row([
-                                html.H4("Input data for regression or classification modeling")
+                                html.H4("Input data for regression or classification modeling")#style={"width": "100%", "display": "flex", "align-items":"center", "justify-content":"center"})
                         ],
-                        style={"height":"10%", "width":"100%", "padding" : "1rem 1rem 1rem 1rem", "border" : "1px solid green"}
+                        style={"height":"15%", "width":"100%", "padding" : "1rem 1rem 1rem 1rem", "border" : "1px solid green", "margin-left" : "0px", "margin-right" : "0px"}
                         ),
 
                         dbc.Row([
+                                dbc.Row([
+
+                                        dbc.Col([
+                                               html.H5("Public service indicator x1", className='text-center align-middle', style={"height":"100%", "width":"100%", "padding" : "1rem 1rem 1rem 1rem"})  
+                                        ],
+                                        style={"height":"100%", "width":"50%", "padding" : "1rem 1rem 1rem 1rem", "border" : "1px solid black", "margin-left" : "0px", "margin-right" : "0px", "vertical-align" : "middle"},
+                                        ),
+
+                                        dbc.Col([
+                                                html.Div([
+                                                        html.P("Indicator"),
+                                                        dcc.RangeSlider(min = 0, max = 20, value=[5, 15], id='my-range-slider', tooltip={"placement": "bottom", "always_visible": True}),
+                                                        html.Div(id='output-container-range-slider'), 
+                                                        ], 
+                                                        className='align-middle',
+                                                        style={"height":"100%", "width":"100%", "padding" : "0px", "box-shadow" : "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"}
+                                                )
+                                        ],
+                                        style={"height":"100%", "width":"50%", "padding" : "1rem 1rem 1rem 1rem", "border" : "1px solid gray", "margin-left" : "0px", "margin-right" : "0px"}
+                                        ),
+                                
+                                        ],
+                                style={"height":"33%", "width":"100%", "padding" : "0px", "border" : "1px solid green", "margin-left" : "0px", "margin-right" : "0px"},      
+                                ),
+                        
+                                dbc.Row([
+                                        dbc.Col([
+                                               html.H5("Public service indicator x2", className='text-center align-middle', style={"height":"100%", "width":"100%", "padding" : "1rem 1rem 1rem 1rem"})  
+                                        ],
+                                        style={"height":"100%", "width":"50%", "padding" : "1rem 1rem 1rem 1rem", "border" : "1px solid black", "margin-left" : "0px", "margin-right" : "0px", "vertical-align" : "middle"},
+                                        ),
+
+                                        dbc.Col([
+                                                html.Div([
+                                                        html.P("Indicator"),
+                                                        dcc.RangeSlider(min = 0, max = 20, value=[5, 15], id='my-range-slider', tooltip={"placement": "bottom", "always_visible": True}),
+                                                        html.Div(id='output-container-range-slider'), 
+                                                        ], 
+                                                        className='align-middle',
+                                                        style={"height":"100%", "width":"100%", "padding" : "0px", "box-shadow" : "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"}
+                                                )
+                                        ],
+                                        style={"height":"100%", "width":"50%", "padding" : "1rem 1rem 1rem 1rem", "border" : "1px solid gray", "margin-left" : "0px", "margin-right" : "0px"}
+                                        ),
+                                
+                                        ],
+                                style={"height":"33%", "width":"100%", "padding" : "0px", "border" : "1px solid red", "margin-left" : "0px", "margin-right" : "0px"}        
+                                ),
+                        
+                                dbc.Row([
+                                        dbc.Col([
+                                               html.H5("Other important indicator", className='text-center align-middle', style={"height":"100%", "width":"100%", "padding" : "1rem 1rem 1rem 1rem"})  
+                                        ],
+                                        style={"height":"100%", "width":"50%", "padding" : "1rem 1rem 1rem 1rem", "border" : "1px solid black", "margin-left" : "0px", "margin-right" : "0px", "vertical-align" : "middle"},
+                                        ),
+
+                                        dbc.Col([
+                                                html.Div([
+                                                        html.P("Indicator"),
+                                                        dcc.RangeSlider(min = 0, max = 20, value=[5, 15], id='my-range-slider', tooltip={"placement": "bottom", "always_visible": True}),
+                                                        html.Div(id='output-container-range-slider'), 
+                                                        ], 
+                                                        className='align-middle',
+                                                        style={"height":"100%", "width":"100%", "padding" : "0px", "box-shadow" : "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"}
+                                                )
+                                        ],
+                                        style={"height":"100%", "width":"50%", "padding" : "1rem 1rem 1rem 1rem", "border" : "1px solid gray", "margin-left" : "0px", "margin-right" : "0px"}
+                                        ),
+                                
+                                        ],
+                                style={"height":"33%", "width":"100%", "padding" : "0px", "border" : "1px solid blue", "margin-left" : "0px", "margin-right" : "0px"}        
+                                ),
+
                                 
                         ]
                         ,
-                        style={"height":"90%", "width":"100%", "padding" : "1rem 1rem 1rem 1rem"}
+                        style={"height":"85%", "width":"100%", "padding" : "0px", "margin-left" : "0px", "margin-right" : "0px"}
                         ),
                 ],
                 style={"padding" : "0px", "height" : "100%", "width" : "100%", "border" : "1px solid blue"}
@@ -39,14 +115,14 @@ page1 = dbc.Container([
                         dbc.Row([
                                 html.H4("Estimated score result after modeling")
                         ],
-                        style={"height":"10%", "width":"100%", "padding" : "1rem 1rem 1rem 1rem", "border" : "1px solid green"}
+                        style={"height":"15%", "width":"100%", "padding" : "1rem 1rem 1rem 1rem", "border" : "1px solid green", "margin-left" : "0px", "margin-right" : "0px"}
                         ),
 
                         dbc.Row([
                                 
                         ]
                         ,
-                        style={"height":"90%", "width":"100%", "padding" : "1rem 1rem 1rem 1rem"}
+                        style={"height":"85%", "width":"100%", "padding" : "0px", "margin-left" : "0px", "margin-right" : "0px"}
                         ),
                 ],
                 ),
@@ -65,7 +141,7 @@ page1 = dbc.Container([
                         dbc.Row([
                                 html.H4("Relationships between public services and ICFES Score")
                         ],
-                        style={"height":"10%", "width":"100%", "padding" : "1rem 1rem 1rem 1rem", "border" : "1px solid green"}
+                        style={"height":"10%", "width":"100%", "padding" : "1rem 1rem 1rem 1rem", "border" : "1px solid green", "margin-left" : "auto", "margin-right" : "auto"}
                         ),
 
                         dbc.Row([
@@ -86,7 +162,7 @@ page1 = dbc.Container([
                         dbc.Row([
                                 html.H4("Relationships between public services and ICFES Score")
                         ],
-                        style={"height":"10%", "width":"100%", "padding" : "1rem 1rem 1rem 1rem", "border" : "1px solid green"}
+                        style={"height":"10%", "width":"100%", "padding" : "1rem 1rem 1rem 1rem", "border" : "1px solid green", "margin-left" : "auto", "margin-right" : "auto"}
                         ),
 
                         dbc.Row([
@@ -113,11 +189,18 @@ fluid=True,
 style={"padding" : "0 0 0 0", "height" : "100%", "width" : "100%"}
 )
 
+@app.callback(
+        Output('output-container-range-slider', 'children'),
+        [Input('my-range-slider', 'value')])
+def update_output(value):
+        return 'You have selected "{}"'.format(value)
+
 class page2:
 
         def render(self):
 
                 return html.Div(page1, style={"padding" : "0 0 0 0", "height" : "100%", "width" : "100%"})
+        
 
 
                 
