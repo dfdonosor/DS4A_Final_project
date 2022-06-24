08efea8d03aed7a0ea050005269eec72e0bc6c1b
Nicolas Acevedo <n.acevedos@uniandes.edu.co>
2022-06-23 20:19:29 -0500

    changes to page 1, adding filters and improving layout

--- pages/page_1.py
+++ pages/page_1.py
@@ -6,10 +6,12 @@ import dash_bootstrap_components as dbc
 import matplotlib.pyplot as plt
 import plotly.express as px
 import pandas as pd
+from components.range_slider import RangerSlider
 
 class page1:
 
         def __init__(self):
+                self.range_slider_1 = RangerSlider("Indicator", "range-slider_1", (2018, 2020))
 
                 self.df = pd.DataFrame({
                 "Year": ["2020", "2020", "2020", "2020", "2020", "2020","2019", "2019", "2019", "2019","2019", "2019", "2018", "2018", "2018", "2018","2018", "2018"],
@@ -39,89 +41,143 @@ class page1:
 
         def render(self):
                 return  dbc.Row([
+                                dbc.Col([
+                                                dbc.Row([
+                                                        dbc.Col(
+                                                                html.Div([
+                                                                        html.P("Municipalities"),
+                                                                        dcc.RangeSlider(min = 2018, max = 2020, step = 1,
+                                                                                        tooltip={"placement": "bottom", "always_visible": True})
+                                                                        ], 
+                                                                        className='align-middle px-2 py-3 m-1 shadow bg-body rounded"'
+                                                                        )
+                                                                )
+                                                        ]),
+                                                dbc.Row([
+                                                        dbc.Col(
+                                                                html.Div([
+                                                                        html.P("hola"),
+                                                                        dcc.Checklist(id="checklist",
+                                                                                        #options=[{"label": x, "value": x} for x in all_countries],
+                                                                                        options=['Tunja', 'Zipaquira', 'Villeta',
+                                                                                        'Villapinzon', 'Villagomez', 'Ubaque',
+                                                                                        'Ubala', 'Tocancipa', 'Tenjo','Tena', 'Susa', 'Suesca',
+                                                                                        'Sibate', 'Cajica', 'Cabrera',
+                                                                                        'Arbelaez', 'Apulo', 'Agua de Dios'],
+                                                                                        #value=all_countries[:3],
+                                                                                        value=['Tunja', 'Zipaquira', 'Villeta'],
+                                                                                        labelStyle={'display': 'block'},
+                                                                                        style={"height":500, "overflow":"auto"}
+                                                                                )
+                                                                        ],
+                                                                        className='align-middle px-2 py-3 m-1 shadow bg-body rounded"'
+                                                                        )
+                                                                )
+                                                        ])
+                                        ], md=2),
                         dbc.Col([
                         dbc.Row([
                                 dbc.Row([
                                         dbc.Col(
-                                                dbc.Card(
+                                                dbc.Card([
                                                         dbc.CardBody([
                                                                 html.H5("Population", className="population"),
                                                                 html.P("10")
                                                                     ])
+                                                        ],
+                                                        class_name='align-middle p-1 mx-1 h-75 mt-1 shadow bg-body rounded"'
                                                         )
                                                 ),
                                         dbc.Col(
-                                                dbc.Card(
+                                                dbc.Card([
                                                         dbc.CardBody([
                                                                 html.H5("Number of Municipalities", className="municipalities"),
                                                                 html.P("20")
                                                                     ])
+                                                        ],
+                                                        class_name='align-middle p-1 mx-1 h-75 mt-1 shadow bg-body rounded"'
                                                         )
                                                 )
                                         ]),
                                 dbc.Row([
                                         dbc.Col(
-                                                dbc.Card(
+                                                dbc.Card([
                                                         dbc.CardBody([
                                                                 html.H5("Global Average Score", className="global_score"),
                                                                 html.P("10")
                                                                     ])
+                                                        ],
+                                                        class_name='align-middle p-1 mx-1 h-75 shadow bg-body rounded"'
                                                         )
                                                 ),
                                         dbc.Col(
-                                                dbc.Card(
+                                                dbc.Card([
                                                         dbc.CardBody([
                                                                 html.H5("Natural Science Average", className="ns_average"),
                                                                 html.P("20")
                                                                     ])
+                                                        ],
+                                                        class_name='align-middle p-1 mx-1 h-75 shadow bg-body rounded"'
                                                         )
                                                 )
                                         ]),
                                 dbc.Row([
                                         dbc.Col(
-                                                dbc.Card(
+                                                dbc.Card([
                                                         dbc.CardBody([
                                                                 html.H5("Math Average", className="math_average"),
                                                                 html.P("10")
                                                                     ])
+                                                        ],
+                                                        class_name='align-middle p-1 mx-1 h-75 shadow bg-body rounded"'
                                                         )
                                                 ),
                                         dbc.Col(
-                                                dbc.Card(
+                                                dbc.Card([
                                                         dbc.CardBody([
                                                                 html.H5("English Average", className="english_average"),
                                                                 html.P("20")
                                                                     ])
+                                                        ],
+                                                        class_name='align-middle p-1 mx-1 h-75 shadow bg-body rounded"'
                                                         )
                                                 )
                                         ]),
                                 dbc.Row([
                                         dbc.Col(
-                                                dbc.Card(
+                                                dbc.Card([
                                                         dbc.CardBody([
                                                                 html.H5("Critical Reading Average", className="cr_average"),
                                                                 html.P("10")
                                                                     ])
+                                                        ],
+                                                        class_name='align-middle p-1 mx-1 h-75 shadow bg-body rounded"'
                                                         )
                                                 ),
                                         dbc.Col(
-                                                dbc.Card(
+                                                dbc.Card([
                                                         dbc.CardBody([
                                                                 html.H5("Social Citizenship Average", className="sc_average"),
                                                                 html.P("20")
                                                                     ])
+                                                        ],
+                                                        class_name='align-middle p-1 mx-1 h-75 shadow bg-body rounded"'
                                                         )
                                                 )
                                         ])
                                 ]),
                                 dbc.Row([
-                                        dbc.Col(
+                                        dbc.Col([
                                                 dcc.Graph(id='example-graph',figure=self.fig)
+                                                ],
+                                                class_name='align-middle p-1 m-1 shadow bg-body rounded"'
                                                 )
                                         ]),
                                 dbc.Row([
-                                        dbc.Col(
+                                        dbc.Col([
                                                 dcc.Graph(id='example-graph',figure=self.fig2)
+                                                ],
+                                                class_name='align-middle p-1 m-1 shadow bg-body rounded"'
                                                 )
                                         ])
                                 ]), 
@@ -129,7 +185,7 @@ class page1:
                                 dbc.Col([
                                         dbc.Row([
                                         dbc.Col(
-                                                dbc.Card(
+                                                dbc.Card([
                                                         dbc.CardBody([
                                                                 html.H5("MAP", className="cr_average"),
                                                                 html.H5("UNDER CONSTRUCTION", className="cr_average"),
@@ -150,21 +206,25 @@ class page1:
                                                                 html.P("------"),
                                                                 html.P("------"),
                                                                 html.P("------"),
-                                                                html.P("------"),
-                                                                html.P("------"),
                                                                 
                                                                 html.P("------")
                                                                     ])
+                                                        ],
+                                                        class_name='align-middle p-1 m-1 shadow bg-body rounded"'
                                                         )
                                                 )
                                         ]),
                                         
                                         dbc.Row([
-                                        dbc.Col(
+                                        dbc.Col([
                                                 dcc.Graph(id='example-graph',figure=self.fig3)
+                                                ],
+                                                class_name='align-middle p-1 m-1 h-75 mt-1 shadow bg-body rounded"'
                                                 )
                                         ])
                                 ])
+
+
                                 ])
                 
                        
