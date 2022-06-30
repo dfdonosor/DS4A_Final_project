ccf56e83c4a4da448136f4f030fc12675b34907f
Daniel Donoso R <dfdonosor@unal.edu.co>
2022-06-29 20:41:28 -0500

    Se agregan los datos y el modelo a la app

---
+++ data/data.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from pycaret.classification import *
+import statsmodels.api as sm
+
+#Open the consolodida_datos
+df = pd.read_pickle("data/consolidado_datos.pickle")
+#Open the modelo_reg
+modelo_reg = sm.load("data\modelo_reg.pickle")
+#Open the modelo_clasificacion
+modelo_classif = load_model("data\modelo_clasificacion_xgboost.pkl")
+
